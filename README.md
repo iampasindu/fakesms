4e700c9f60bd9bfa9ba846f668e6b474fe64a996
iampasindu <102863346+iampasindu@users.noreply.github.com>
2022-05-10 12:10:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# fakesms
-SEND FAKE SMS TO ANY NUMBER
+<h1 align="center">Anon-SMS v1.4</h1>
+<p align="center">Anonymous Message Sender for Linux And Termux.</p><br>
+
+## Updates:
+**v1.0**<br>
+[+] Basic Features<br>
+[+] Non-Colourful Interface.<br><br>
+**v1.1**<br>
+[+] Adding Colours.<br><br>
+**v1.2**<br>
+[+] Fixing Colour Issues<br>
+[+] Adding Tracking Feature.<br><br>
+**v1.3**<br>
+[+] Adding Lolcat To Make More Colourful Start.<br><br>
+**v1.4**<br>
+[+] Adding Auto-Update Feature.<br>
+[+] Adding Telegram Discussion.<br>
+
+## Note:
+
+- The script requires working network connection to work.
+- No balance will be deducted for using this script to send SMS.
+- Don't put spaces in between phone number (Ex- 99999 99999)
+- Make sure you are using the latest version of Anon-SMS
+- Make sure you are using Python v3.
+
+Here's how you can check it. Type this command in your terminal.
+```
+$ python -V
+```
+If output looks like `Python 3` or above - Congrats, Python 3 is installed properly.
+
+- Do not use this to harm others.
+- This script is only for educational purposes or to prank.
+- **None of the developers/contributors are responsible for the misuse of Anon-SMS.**
+<br>
+
+## Features:
+
+- This Is A Free Version.
+- Super-fast Mesage Sending.
+- International Message Sending available.
+- You Can Send Only One Message Per Day.
+- Messaging Anonymously.
+- Easy to use and embed in code.
+
+## Usage:
+
+Run these commands to Send Anonymous Message
+
+### > For Termux:
+
+**Notice:** 
+
+git installation methods are not universal and do differ between distributions,
+so, installing git as per instructions below may not work.
+Please check out how to install `git` for your Linux distribution.
+Commands below provide instructions for Debian-based systems.
+
+To Send Messages Anonymously type the following commands in Termux:
+```
+pkg install git
+pkg install python
+git clone https://github.com/iampasindu/fakesms.git
+cd Anon-SMS
+bash Run.sh
+```
+
+### > For Linux:
+
+**Notice:** 
+
+git installation methods are not universal and do differ between distributions,
+so, installing git as per instructions below may not work.
+Please check out how to install `git` for your Linux distribution.
+Commands below provide instructions for Debian-based systems.
+
+To Send Message Anonymously type the following commands in Linux terminal:
+```
+sudo apt install git
+git clone https://github.com/iampasindu/fakesms.git
+cd Anon-SMS
+sudo bash Run.sh
+```
+## CONTRIBUTORS:
+
+
+
+# CONTACT ME:
+
+Feel Free To Open An Issue...
+
+```
+                 
+      YouTube Channel: https://bit.ly/37uxmUu
+```
+
+<a href="https://bit.ly/37uxmUu">Check Out Our YouTube Channel</a><br>
+<a href="https://www.instagram.com/_.pasindu/">Check Out Our instagram</a><br>
+
---
+++ Run.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+
+clear
+echo -e "\e[4;31m SL CODING LAB !!! \e[0m"
+echo -e "\e[1;32m Presents \e[0m"
+echo -e "\e[1;34m"
+figlet -f slant Anon-SMS
+echo "Press Enter To Launch"
+read a1
+if [[ -s update.pandahackers ]];then
+echo "All Requirements Found...."
+else
+echo 'Installing Requirements....'
+echo .
+echo .
+apt install figlet toilet python curl ruby -y
+apt install python3-pip
+gem install lolcat
+echo This Tool Is Created By SL CODING HUB
+echo This Script Is Created By SL CODING HUB HACK3RY2 >update.pandahackers
+echo Requirements Installed....
+echo Press Enter To Continue...
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+figlet -f slant Anon-SMS | lolcat
+echo -e "\e[1;34m Created By \e[1;32m"  #blue color
+toilet -f mono12 -F border PH | lolcat
+echo -e "\e[4;34mThis Tool Is Created By SL CODING HUB \e[0m" #underline+blue
+echo -e "\e[1;34mFor Any Queries Mail Us!!!\e[0m"
+echo -e "\e[1;32m       Instagram: https://www.instagram.com/_.pasindu/ \e[0m" #yellow
+echo -e "\e[4;32mYouTube Page: https://www.youtube.com/channel/UCPGqhXiqeAkO4vX4E9I23HQ \e[0m" #green
+echo " "
+echo -e "\e[4;31mRead Instruction Carefully !!! \e[0m" #red
+echo " "
+echo "Enter 1 To  Run Anon-SMS "
+echo "Enter 2 To  Track Anon-SMS "  #white
+echo "Enter 3 To  Update "
+echo "Enter 4 To  Check Features "
+echo "Enter 5 To  Exit "
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 send.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Track Anon-SMS'> call.xxx
+python3 send.py track
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Requesting Update From Source..."
+echo -e "\e[1;34m Request Attempt Successful.."
+echo -e "\e[1;34m Updating Now..."
+git clone https://github.com/iampasindu/fakesms.git
+if [[ -s Anon-SMS/Run.sh ]];then
+cd Anon-SMS
+cp -r -f * .. > temp
+cd ..
+rm -rf  Anon-SMS >> temp
+rm update.pandahackers >> temp
+rm temp
+chmod +x Run.sh
+fi
+echo -e "\e[1;32m Anon-SMS Will Refresh Now..."
+echo -e "\e[1;32m All The Required Packages Have Been Installed..."
+echo -e "\e[1;34m Press Enter To Refresh..."
+read a6
+./Run.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;33m"
+figlet -f slant Anon-SMS | lolcat
+echo -e "\e[1;34mCreated By \e[1;34m"  #Blue Color
+toilet -f mono12 -F border PH | lolcat
+echo  " "
+echo -e "\e[1;32m                   Features\e[1;34m"
+echo "  Ultra-Fast-Smooth Sending"
+echo "  International Sending"
+echo "  Tracking "
+echo "  Automatic Future Updates"
+echo "  Easy To Use And Embed in Code"
+echo""
+echo -e "\e[1;32m                   Contributors\e[1;33m"
+echo -e "\e[1;33m      [*]  SL CODING LAB   \e[1;31m"
+echo "         [-] Mail At: https://www.youtube.com/channel/UCPGqhXiqeAkO4vX4E9I23HQ"
+echo -e "\e[1;33m      [*]  Instagram   \e[1;31m"
+echo "         [-] Ping At: https://www.instagram.com/_.pasindu/"
+echo -e "\e[1;33m      [*]  HAOI      \e[1;31m"
+echo "         [-] Ping At: https://www.instagram.com/_.pasindu/"
+echo ""
+echo ""
+echo -e "\e[1;31m This is Only For Educational Purposes or To Prank.\e[0m"
+echo -e "\e[1;31m Do not Use This To Irritate Others. \e[0m"
+echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
+echo -e "\e[1;31m We Are Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;32m Update If Doesn't Work.\e[0m"
+echo  " "
+echo -e "\e[4;31m That's All !!!\e[0m"
+echo -e "\e[1;34m For Any Queries Mail Us!!!\e[0m"
+echo -e "\e[1;32m   Instagram: https://www.instagram.com/_.pasindu/ \e[0m"
+echo -e "\e[4;32mYouTube Page: https://www.youtube.com/channel/UCPGqhXiqeAkO4vX4E9I23HQ \e[0m"
+echo "Press Enter To Return To Main Menu"
+read a3
+clear
+elif [ $ch -eq 5 ];then
+clear
+echo -e "\e[1;31m"
+figlet -f slant Anon-SMS | lolcat
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f pagga -F border HACK3RY2J | lolcat
+echo -e "\e[1;34m For Any Queries Instagram Us!!!\e[0m"
+echo -e "\e[1;32m        Instagram: https://www.instagram.com/_.pasindu/ \e[0m"
+echo -e "\e[4;32mYouTube Page: https://www.youtube.com/channel/UCPGqhXiqeAkO4vX4E9I23HQ \e[0m"
+echo " "
+exit 0
+else
+echo -e "\e[4;32m Invalid Input Detected !!! \e[0m"
+echo "Press Enter To Return To Main Menu"
+read a3
+clear
+fi
+done
